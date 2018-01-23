80a384cde71e812fc4b808f0b42b69faa841dcdf
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-23 22:09:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,14 @@ class Dog
 
   attr_accessor :mood
   attr_reader :name
-
+      @@all = []
   def initialize(name)
     @name = name
     @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
