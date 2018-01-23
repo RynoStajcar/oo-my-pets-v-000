a701e9e56fc4ed4c1bb75b43d91864ebdcbefcf6
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-23 22:09:39 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -2,9 +2,14 @@ class Fish
 
   attr_accessor :mood
   attr_reader :name
-
+      @@all = []
   def initialize(name)
     @name = name
     @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
